fb4bb0b01dfbae7dfae2b46c3296dde5f3c273ce
N U R O ™ <bendixon50@gmail.com>
2019-01-02 11:09:32 +0000

    :tada: Initial project commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+import App from './App.vue';
+import store from './store';
+
+import './plugins/vuetify';
+
+Vue.config.productionTip = false;
+
+new Vue({
+	store,
+	render: (h) => h(App),
+}).$mount('#app');
