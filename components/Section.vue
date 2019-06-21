dc284e87cc91eb87962eba29aa2caca3c45852d8
N U R O ™ <bendixon50@gmail.com>
2019-06-21 18:54:45 +0100

    :art: Refactored sections to use section base component

---
+++ components/Section.vue
@@ -0,0 +1,30 @@
+<template>
+	<section :id="id" :class="dark ? 'angle' : null">
+		<v-layout align-center row wrap :class="`py-5 ${dark ? 'primary' : 'transparent'}`">
+			<v-flex xs12>
+				<v-container grid-list-xl>
+					<v-layout row wrap align-center>
+						<slot name="content" />
+					</v-layout>
+				</v-container>
+			</v-flex>
+		</v-layout>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		id: {
+			type: String,
+			default: '',
+			required: true,
+		},
+		dark: {
+			type: Boolean,
+			default: false,
+			required: false,
+		},
+	},
+};
+</script>
--- components/Sections/Extensions.vue
+++ components/Sections/Extensions.vue
@@ -1,55 +1,50 @@
 <template>
-	<section id="extensions">
-		<v-layout align-center row wrap class="py-5 transparent">
-			<v-flex xs12>
-				<v-container grid-list-xl>
-					<v-layout row wrap align-center>
-						<v-flex xs12 sm12 md4>
-							<v-card class="elevation-0 transparent" data-aos="fade-right">
-								<v-card-text primary-title class="text-xs-center">
-									<IconExtensions width="64px" height="64px" />
-									<div
-										class="primary--text display-3 text-xs-center font-weight-black text-uppercase"
-									>
-										Extensions
-									</div>
-									<div
-										class="primary--text caption text-spaced text-xs-center text-uppercase font-weight-thin"
-									>
-										Section subheading text goes here
-									</div>
-								</v-card-text>
-								<v-card-text class="primary--text">
-									Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat
-									tincidunt ornare. Pellentesque habitant morbi tristique senectus
-									et netus et malesuada fames ac turpis egestas. Nullam in aliquet
-									odio. Aliquam eu est vitae tellus bibendum tincidunt.
-									Suspendisse potenti.
-								</v-card-text>
-							</v-card>
-						</v-flex>
-						<v-flex xs12 sm12 md8>
-							<v-card
-								class="ma-3 transparent elevation-0"
-								data-aos="fade-left"
-								data-aos-delay="200"
-							>
-								<v-responsive height="40vh" class="grey lighten-3" />
-							</v-card>
-						</v-flex>
-					</v-layout>
-				</v-container>
+	<Section id="extensions">
+		<template slot="content">
+			<v-flex xs12 sm12 md4>
+				<v-card class="elevation-0 transparent" data-aos="fade-right">
+					<v-card-text primary-title class="text-xs-center">
+						<IconExtensions width="64px" height="64px" />
+						<div
+							class="primary--text display-3 text-xs-center font-weight-black text-uppercase"
+						>
+							Extensions
+						</div>
+						<div
+							class="primary--text caption text-spaced text-xs-center text-uppercase font-weight-thin"
+						>
+							Section subheading text goes here
+						</div>
+					</v-card-text>
+					<v-card-text class="primary--text">
+						Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt
+						ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada
+						fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus
+						bibendum tincidunt. Suspendisse potenti.
+					</v-card-text>
+				</v-card>
 			</v-flex>
-		</v-layout>
-	</section>
+			<v-flex xs12 sm12 md8>
+				<v-card
+					class="ma-3 transparent elevation-0"
+					data-aos="fade-left"
+					data-aos-delay="200"
+				>
+					<v-responsive height="40vh" class="grey lighten-3" />
+				</v-card>
+			</v-flex>
+		</template>
+	</Section>
 </template>
 
 <script>
 import IconExtensions from '~/static/icons/extensions.svg';
+import Section from '~/components/Section';
 
 export default {
 	components: {
 		IconExtensions,
+		Section,
 	},
 };
 </script>
