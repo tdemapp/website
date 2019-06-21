e0467211337026bdb415d1637e568ca6a22fe6c6
N U R O ™ <bendixon50@gmail.com>
2019-06-21 18:25:05 +0100

    :rotating_light: Linter fixes

--- pages/faq.vue
+++ pages/faq.vue
@@ -10,11 +10,12 @@
 							data-aos="fade-up"
 							:data-aos-offset="-50 * i"
 						>
-							<v-card-title
-								primary-title
-							>
+							<v-card-title primary-title>
 								<span class="display-1 mr-3" v-text="question.emoji" />
-								<span class="headline font-weight-black text-uppercase" v-text="question.title" />
+								<span
+									class="headline font-weight-black text-uppercase"
+									v-text="question.title"
+								/>
 							</v-card-title>
 
 							<v-divider class="mx-2" />
@@ -35,23 +36,26 @@ export default {
 	metaInfo: {
 		title: 'FAQ',
 	},
-	data () {
+	data() {
 		return {
 			questions: [
 				{
 					emoji: '❓',
 					title: 'What is TDEM',
-					body: 'TDEM (TweetDeck Extension Manager) is a browser extension designed to provide a simple and easy way to install and manage extensions to enhance Twitter\'s TweetDeck client.',
+					body:
+						"TDEM (TweetDeck Extension Manager) is a browser extension designed to provide a simple and easy way to install and manage extensions to enhance Twitter's TweetDeck client.",
 				},
 				{
 					emoji: '🏗',
 					title: 'How does it work',
-					body: 'TDEM is build using a fork of the TD.Extensions specification built by the DeckHack team. ❤️ HUGE thanks to them for their work on creating the specification.',
+					body:
+						'TDEM is build using a fork of the TD.Extensions specification built by the DeckHack team. ❤️ HUGE thanks to them for their work on creating the specification.',
 				},
 				{
 					emoji: '🤝',
 					title: 'Are you affiliated with Twitter',
-					body: '**NO**. TDEM is in no way allifiated with Twitter or TweetDeck in anyway. ',
+					body:
+						'**NO**. TDEM is in no way allifiated with Twitter or TweetDeck in anyway. ',
 				},
 				{
 					emoji: '️💎',
@@ -61,25 +65,29 @@ export default {
 				{
 					emoji: '📦',
 					title: 'How do I make an extension',
-					body: 'Anyone can create an extension for TDEM. You can find both a JavaScript and a TypeScript template to build an extension on our GitHub. We also provide multiple example extensions you can use as reference to help create your own extension',
+					body:
+						'Anyone can create an extension for TDEM. You can find both a JavaScript and a TypeScript template to build an extension on our GitHub. We also provide multiple example extensions you can use as reference to help create your own extension',
 				},
 				{
 					emoji: '🔥',
-					title: 'An extension isn\'t working, what do I do',
-					body: 'TweetDeck can often make changes that can result in extensions breaking/not working as intended. We recommend first checking your TweetDeck console to try and narrow down the extension causing the issues. From there we recommend contacting the author of the extension to let them investogate the issue and if need be release an updated version.',
+					title: "An extension isn't working, what do I do",
+					body:
+						'TweetDeck can often make changes that can result in extensions breaking/not working as intended. We recommend first checking your TweetDeck console to try and narrow down the extension causing the issues. From there we recommend contacting the author of the extension to let them investogate the issue and if need be release an updated version.',
 				},
 				{
 					emoji: '⬇️',
 					title: 'Where can I download extensions from',
-					body: 'TDEM offers a built in "Store" that is connected to our public extension repository and offers a simple and easy way to download and install verified extensions.',
+					body:
+						'TDEM offers a built in "Store" that is connected to our public extension repository and offers a simple and easy way to download and install verified extensions.',
 				},
 				{
 					emoji: '⚠️',
 					title: 'Can I install extensions from a 3rd party',
-					body: 'We do offer the ability to download extensions via a url, however this method is highly discouraged unless you know what you are doing. Downloading and installing 3rd party extensions means that they have most likely not been verified by the community and as such could be malicious. If you do want to install a 3rd party extension we recommend reviewing the source code for the extension first before installing it to be safe.'
+					body:
+						'We do offer the ability to download extensions via a url, however this method is highly discouraged unless you know what you are doing. Downloading and installing 3rd party extensions means that they have most likely not been verified by the community and as such could be malicious. If you do want to install a 3rd party extension we recommend reviewing the source code for the extension first before installing it to be safe.',
 				},
-			]
-		}
+			],
+		};
 	},
 	head() {
 		return {
