d6ce67605b371d6c7400e182da2e6f82758349f2
N U R O ™ <bendixon50@gmail.com>
2019-06-24 12:35:54 +0100

    :art: Cleaned up nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
-const downloadUrls = {
+const title = 'TDEM';
+const description = 'TweetDeck Extension Manager';
+const titleFull = title + ' | ' + description;
+const downloads = {
 	chrome: 'https://chrome.google.com/webstore',
 	firefox: 'https://addons.mozilla.org',
 	opera: 'https://addons.opera.com',
@@ -6,90 +9,76 @@ const downloadUrls = {
 
 module.exports = {
 	mode: 'spa',
-
-	// Headers of the page
 	head: {
-		title: 'TDEM | TweetDeck Extension Manager',
+		title: titleFull,
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: 'TweetDeck Extension Manager' },
+			{ hid: 'description', name: 'description', content: description },
 		],
 		link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }],
 	},
-
-	// Customize the progress-bar color
 	loading: {
 		color: '#ffffff',
 		failedColor: '#f44336',
 		height: '3px',
 	},
-
-	// Nuxt PWA module
-	meta: {
-		favicon: true,
-		name: 'TDEM | TweetDeck Extension Manager',
-		description: 'TweetDeck Extension Manager',
-		lang: 'en',
-		theme_color: '#212121',
-	},
-
-	// Manifest data
-	manifest: {
-		name: 'TDEM',
-		short_name: 'TDEM',
-		description: 'TweetDeck Extension Manager',
-		lang: 'en',
-		start_url: '/',
-		display: 'standalone',
-		orientation: 'portrait',
-		background_color: '#212121',
-		theme_color: '#212121',
-	},
-
-	// Icon sizes to generate
-	icon: {
-		sizes: [16, 128, 144, 152, 192, 256, 512],
-	},
-
-	// Nuxt Robots modules
-	robots: [
-		{
-			UserAgent: '*',
-			Disallow: '',
-		},
+	css: [
+		'~/assets/style/app.styl'
 	],
-	// Nuxt Markdown parser
-	markdownit: {
-		injected: true,
-	},
-
-	// URL redirects
-	redirect: [
-		{ from: '^/chrome', to: downloadUrls.chrome, statusCode: 301 },
-		{ from: '^/firefox', to: downloadUrls.firefox, statusCode: 301 },
-		{ from: '^/opera', to: downloadUrls.opera, statusCode: 301 },
+	plugins: [
+		'@/plugins/vuetify',
+		'@/plugins/aos'
 	],
-
-	// Global CSS
-	css: ['~/assets/style/app.styl'],
-
-	// Plugins to load before mounting the App
-	plugins: ['@/plugins/vuetify', '@/plugins/aos'],
-
-	// Nuxt.js modules
 	modules: [
-		'@nuxtjs/markdownit',
-		'@nuxtjs/redirect-module',
-		'@nuxtjs/robots',
-		'@nuxtjs/pwa',
-		'svg-to-vue-component/nuxt',
+		[
+			'@nuxtjs/markdownit',
+			{
+				injected: true,
+			}
+		],
+		[
+			'@nuxtjs/redirect-module',
+			[
+				{ from: '^/chrome', to: downloads.chrome, statusCode: 301 },
+				{ from: '^/firefox', to: downloads.firefox, statusCode: 301 },
+				{ from: '^/opera', to: downloads.opera, statusCode: 301 },
+			]
+		],
+		[
+			'@nuxtjs/robots',
+			{
+				UserAgent: '*',
+				Disallow: '',
+			}
+		],
+		[
+			'@nuxtjs/pwa',
+			{
+				meta: {
+					favicon: true,
+					name: titleFull,
+					description: description,
+					lang: 'en',
+					theme_color: '#212121',
+				},
+				manifest: {
+					name: title,
+					short_name: title,
+					description: description,
+					lang: 'en',
+					start_url: '/',
+					display: 'standalone',
+					orientation: 'portrait',
+					background_color: '#212121',
+					theme_color: '#212121',
+				},
+				icon: {
+					sizes: [16, 128, 144, 152, 192, 256, 512],
+				},
+			}
+		],
+		'nuxt-svg-loader',
 		'vue-scrollto/nuxt',
 	],
-
-	// Build configuration
-	build: {
-		// You can extend webpack config here
-		extend(config, ctx) {},
-	},
 };
