76107c6875b53eee8126da40f28433b58bd4524e
N U R O ™ <bendixon50@gmail.com>
2019-06-24 12:44:32 +0100

    :zap: Added Vuetify component tree shaking

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,10 +1,53 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vuetify, {
+	VApp,
+	VAvatar,
+	VBtn,
+	VCard,
+	VCardActions,
+	VCardTitle,
+	VCardText,
+	VContent,
+	VContainer,
+	VDialog,
+	VDivider,
+	VFlex,
+	VFooter,
+	VLayout,
+	VResponsive,
+	VSpacer,	
+	VToolbar,
+	VToolbarItems,
+} from 'vuetify/lib';
+import { Ripple } from 'vuetify/lib/directives';
 
 import 'vuetify/dist/vuetify.min.css';
 import 'vuetify/src/stylus/app.styl';
 
 Vue.use(Vuetify, {
+	components: {
+		VApp,
+		VAvatar,
+		VBtn,
+		VCard,
+		VCardActions,
+		VCardTitle,
+		VCardText,
+		VContent,
+		VContainer,
+		VDialog,
+		VDivider,
+		VFlex,
+		VFooter,
+		VLayout,
+		VResponsive,
+		VSpacer,
+		VToolbar,
+		VToolbarItems,
+	},
+	directives: {
+		Ripple,
+	},
 	theme: {
 		primary: '#111111',
 		secondary: '#161616',
