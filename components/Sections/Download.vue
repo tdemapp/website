6c19dd0c917a674097360b35e39c8d04c5a0abdf
N U R O ™ <bendixon50@gmail.com>
2019-06-25 20:05:54 +0100

    :lipstick: Redesigned downloads section

--- components/Sections/Download.vue
+++ components/Sections/Download.vue
@@ -1,86 +1,59 @@
 <template>
 	<section id="download">
 		<v-layout align-center justify-center row wrap class="py-5 transparent">
-			<v-flex xs12 md4>
-				<v-container grid-list-xl>
-					<v-layout row wrap align-center>
-						<v-flex xs12>
-							<v-card class="elevation-0 transparent">
-								<v-card-text
-									class="py-0 text-xs-center"
-									data-aos="fade-down"
-									data-aos-easing="ease-in-out-back"
-								>
-									<h1
-										class="primary--text display-3 text-spaced header text-xs-center font-weight-black text-uppercase"
-									>
-										Download
-									</h1>
-								</v-card-text>
-
-								<v-card-actions
-									class="mx-2"
-									data-aos="zoom-in"
-									data-aos-delay="50"
-									data-aos-easing="ease-in-out-back"
-								>
-									<v-btn
-										large
-										outline
-										round
-										block
-										class="headerBtn"
-										color="blue"
-										href="/chrome"
-									>
-										<IconDownload class="mr-3" />
-										Chrome
-									</v-btn>
-								</v-card-actions>
-
-								<v-card-actions
-									class="mx-2"
-									data-aos="zoom-in"
-									data-aos-delay="150"
-									data-aos-easing="ease-in-out-back"
-								>
-									<v-btn
-										large
-										outline
-										round
-										block
-										class="headerBtn"
-										color="deep-orange"
-										href="/firefox"
-									>
-										<IconDownload class="mr-3" />
-										Firefox
-									</v-btn>
-								</v-card-actions>
-
-								<v-card-actions
-									class="mx-2"
-									data-aos="zoom-in"
-									data-aos-delay="250"
-									data-aos-easing="ease-in-out-back"
-								>
-									<v-btn
-										large
-										outline
-										round
-										block
-										class="headerBtn"
-										color="red"
-										href="/opera"
-									>
-										<IconDownload class="mr-3" />
-										Opera
-									</v-btn>
-								</v-card-actions>
-							</v-card>
-						</v-flex>
-					</v-layout>
-				</v-container>
+			<v-flex xs12 sm8 md3 lg2 class="ma-4" data-aos="zoom-in" data-aos-easing="ease-in-out-back">
+				<v-card dark href="/chrome" class="elevation-0 blue darken-2 downloadCard">
+					<v-img class="blue">
+						<v-card-title class="py-3">
+							<v-spacer />
+								<IconDownload class="white--text ma-2" width="64px" height="64px" />
+							<v-spacer />
+						</v-card-title>
+					</v-img>
+					<v-card-title primary-title class="pa-3 text-xs-center">
+						<v-spacer />
+						<h1 class="text-spaced font-weight-black text-uppercase">
+							Chrome
+						</h1>
+						<v-spacer />
+					</v-card-title>
+				</v-card>
+			</v-flex>
+			<v-flex xs12 sm8 md3 lg2 class="ma-4" data-aos="zoom-in" data-aos-delay="100" data-aos-easing="ease-in-out-back">
+				<v-card dark href="/firefox" class="elevation-0 deep-orange darken-2 downloadCard">
+					<v-img class="deep-orange">
+						<v-card-title class="py-3">
+							<v-spacer />
+								<IconDownload class="white--text ma-2" width="64px" height="64px" />
+							<v-spacer />
+						</v-card-title>
+					</v-img>
+					<v-card-title primary-title class="pa-3 text-xs-center">
+						<v-spacer />
+						<h1 class="text-spaced font-weight-black text-uppercase">
+							Firefox
+						</h1>
+						<v-spacer />
+					</v-card-title>
+				</v-card>
+			</v-flex>
+			<v-flex xs12 sm8 md3 lg2 class="ma-4" data-aos="zoom-in" data-aos-delay="200" data-aos-easing="ease-in-out-back">
+				<v-card dark href="/opera" class="elevation-0 red darken-2 downloadCard">
+					<v-img class="red">
+						<v-card-title class="py-3">
+							<v-spacer />
+								<IconDownload class="white--text ma-2" width="64px" height="64px" />
+							<v-spacer />
+						</v-card-title>
+					</v-img>
+					<v-card-title primary-title class="pa-3 text-xs-center">
+						<v-spacer />
+						<h1 class="text-spaced font-weight-black text-uppercase">
+							Opera
+						</h1>
+						<v-spacer />
+					</v-card-title>
+				</v-card>
 			</v-flex>
 		</v-layout>
 	</section>
@@ -95,3 +68,18 @@ export default {
 	},
 };
 </script>
+
+<style>
+.downloadCard {
+	transition: all 0.5s;
+}
+.downloadCard:hover {
+	transition: all 0.5s;
+	box-shadow: 0 8px 15px rgba(33, 33, 33, 0.1) !important;
+	transform: translateY(-4px)!important;
+}
+.downloadCard:active {
+	transition: all 0.5s;
+	transform: translateY(2px)!important;
+}
+</style>
