78acf5074b8ec026581edc8430a851f25e5cd61d
N U R O ™ <me@nuro.dev>
2021-01-29 02:34:13 +0000

    Added Splitbee analytics plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Pages from 'vite-plugin-pages';
+import { ViteSplitbee } from 'vite-plugin-splitbee';
 
 export default defineConfig({
 	plugins: [
 		Pages(),
-		vue()
+		ViteSplitbee(),
+		vue(),
 	],
 });
