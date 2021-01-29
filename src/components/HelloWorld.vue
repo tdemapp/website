7f735bce2cc3a30c17781d314df6266e801f7004
N U R O ™ <me@nuro.dev>
2021-01-29 00:24:18 +0000

    Added initial vite project structure

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+	<h1>{{ msg }}</h1>
+
+	<p>
+		<a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+		<a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+	</p>
+
+	<p>
+		Recommended setup:
+		<a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+		+
+		<a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank"
+			>Vetur</a
+		>
+		+
+		<a
+			href="https://marketplace.visualstudio.com/items?itemName=znck.vue-language-features"
+			target="_blank"
+			>Vue DX</a
+		>
+	</p>
+	<p>
+		Make sure to use workspace version of TypeScript to get improved support via
+		<a href="https://github.com/znck/vue-developer-experience" target="_blank">@vuedx</a>.
+		<br />Note @vuedx is still experimental and this setup is provided for early feedback.
+	</p>
+	<button @click="increment">count is: {{ count }}</button>
+	<p>
+		Edit
+		<code>components/HelloWorld.vue</code> to test hot module replacement.
+	</p>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue';
+
+const props = defineProps({
+	msg: {
+		type: String,
+		required: true,
+	},
+});
+
+const count = ref(0);
+
+const increment = () => count.value++;
+</script>
+
+<style scoped>
+a {
+	color: #42b983;
+}
+</style>
