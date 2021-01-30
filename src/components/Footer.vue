18227a590e3a2820d6ff5c59e6f0e599326a1713
nurodev <me@nuro.dev>
2021-01-30 09:32:26 +0000

    Added Vercel link to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,27 +1,18 @@
 <template>
-	<footer class="mt-24 bg-gray-900 sm:mt-12">
+	<footer class="bg-gray-900">
 		<div
-			class="mx-auto max-w-md py-12 px-4 overflow-hidden sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8"
+			class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8"
 		>
-			<nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-				<div class="px-5 py-2">
-					<a href="#" class="text-base text-gray-400 hover:text-gray-300"> About </a>
-				</div>
+			<div class="flex justify-center space-x-6 md:order-3">
+				<a href="https://vercel.com/home?utm_source=tdem&utm_campaign=oss" target="_blank" rel="noopener noreferrer">
+					<svg width="107" height="24" viewBox="0 0 283 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM248.72 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM200.24 34c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9V5h9zM36.95 0L73.9 64H0L36.95 0zm92.38 5l-27.71 48L73.91 5H84.3l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10V51h-9V17h9v9.2c0-5.08 5.91-9.2 13.2-9.2z" fill="#fff"/>
+					</svg>
+				</a>
+			</div>
 
-				<div class="px-5 py-2">
-					<a href="#" class="text-base text-gray-400 hover:text-gray-300"> FAQ </a>
-				</div>
-
-				<div class="px-5 py-2">
-					<a href="#" class="text-base text-gray-400 hover:text-gray-300"> Releases </a>
-				</div>
-
-				<div class="px-5 py-2">
-					<a href="#" class="text-base text-gray-400 hover:text-gray-300"> Disclaimer </a>
-				</div>
-			</nav>
-			<div class="mt-8 flex justify-center space-x-6">
-				<a href="#" class="text-gray-400 hover:text-gray-300">
+			<div class="flex justify-center space-x-6 md:order-2">
+				<a href="#" class="text-gray-400 hover:text-white transition ease-in-out duration-300">
 					<span class="sr-only">Twitter</span>
 					<svg
 						class="h-6 w-6"
@@ -41,7 +32,7 @@
 					</svg>
 				</a>
 
-				<a href="#" class="text-gray-400 hover:text-gray-300">
+				<a href="#" class="text-gray-400 hover:text-white transition ease-in-out duration-300">
 					<span class="sr-only">GitHub</span>
 					<svg
 						class="h-6 w-6"
@@ -61,9 +52,12 @@
 					</svg>
 				</a>
 			</div>
-			<p class="mt-8 text-center text-base text-gray-400">
-				&copy; {{ new Date().getFullYear() }} Workflow, Inc. All rights reserved.
-			</p>
+
+			<div class="mt-8 md:mt-0 md:order-1">
+				<p class="text-center text-base text-gray-400 select-none">
+					&copy; {{ new Date().getFullYear() }} Workflow, Inc.
+				</p>
+			</div>
 		</div>
 	</footer>
 </template>
