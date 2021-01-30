f3749d303c1ffecc960e30107d73cf5b0fe5f2de
nurodev <me@nuro.dev>
2021-01-30 10:01:17 +0000

    Added WIP download component

---
+++ src/components/Download.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+		<div class="mt-6 grid grid-cols-2 gap-4 md:grid-cols-3 lg:mt-8">
+			<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50" data-aos="zoom-in" data-aos-easing="ease-in-out-back">
+				<img
+					class="max-h-12"
+					src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+					alt="Workcation"
+				/>
+			</div>
+			<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50" data-aos="zoom-in" data-aos-easing="ease-in-out-back" data-aos-delay="100">
+				<img
+					class="max-h-12"
+					src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
+					alt="Mirage"
+				/>
+			</div>
+			<div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50" data-aos="zoom-in" data-aos-easing="ease-in-out-back" data-aos-delay="200">
+				<img
+					class="max-h-12"
+					src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
+					alt="Tuple"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,11 +3,13 @@
 	<Hero />
 	<Features />
 	<CTA />
+	<Download />
 </template>
 
 <script setup lang="ts">
 import Banner from '../components/Banner.vue';
 import CTA from '../components/CTA.vue';
+import Download from '../components/Download.vue';
 import Features from '../components/Features.vue';
 import Hero from '../components/Hero.vue';
 </script>
